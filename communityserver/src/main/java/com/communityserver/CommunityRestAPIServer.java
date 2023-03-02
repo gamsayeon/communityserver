21f0bfc43c26a93bf431c4a49dd15b017708d534
gamsayeon <aud4551@naver.com>
2023-03-02 21:11:18 +0900

    spring boot 프로젝트 생성

---
+++ communityserver/src/main/java/com/communityserver/CommunityRestAPIServer.java
@@ -0,0 +1,13 @@
+package com.communityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityRestAPIServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityRestAPIServer.class, args);
+	}
+
+}
---
+++ communityserver/src/test/java/com/communityserver/CommunityRestAPIServerTests.java
@@ -0,0 +1,13 @@
+package com.communityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityRestAPIServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
